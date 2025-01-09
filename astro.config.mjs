d960ce416cfff30165fa52d3d9760269569a761c
Ricardo Contreras <40666926+rcrdevelop@users.noreply.github.com>
2025-01-09 15:05:01 +0000

    init deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from '@astrojs/vercel';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel(),
 });
